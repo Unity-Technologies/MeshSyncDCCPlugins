2b5d6c6104abbb56b8c5f7eca04a04ede2bfeae0
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-03-09 12:30:42 +0900

    Check if downloading meshsync package is successful

--- Build/cmake_modules/MeshSync.cmake
+++ Build/cmake_modules/MeshSync.cmake
@@ -10,7 +10,16 @@ function(get_meshsync meshsync_ver)
    if(NOT EXISTS ${MESHSYNC_ARCHIVE_LOCAL_PATH})
        message("Downloading MeshSync " ${meshsync_ver})
        file(DOWNLOAD ${MESHSYNC_ARCHIVE_URL} ${MESHSYNC_ARCHIVE_LOCAL_PATH} SHOW_PROGRESS)
+
+       # Check if the download is successful
+       file(SIZE ${MESHSYNC_ARCHIVE_LOCAL_PATH} MESHSYNC_ARCHIVE_FILESIZE)
+       if(0 EQUAL ${MESHSYNC_ARCHIVE_FILESIZE})
+           file(REMOVE ${MESHSYNC_ARCHIVE_LOCAL_PATH})
+           message(FATAL_ERROR "Could not download MeshSync ${meshsync_ver} !")
+       endif()
    endif()
+
+   
    execute_process(
        WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
        COMMAND tar -xf ${MESHSYNC_ARCHIVE_LOCAL_PATH}
