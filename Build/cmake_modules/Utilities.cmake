531196fdae64d75c6bd8a431c245277403ec9365
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-03-13 12:11:10 +0900

    Add an Utilities cmake file to store common utilities

---
+++ Build/cmake_modules/Utilities.cmake
@@ -0,0 +1,31 @@
+# Various utilities for cmake
+
+function(download_and_extract 
+            archive_url
+            archive_local_path 
+            download_msg
+            err_msg
+        )
+    if(NOT EXISTS ${archive_local_path})
+        message(${download_msg})
+        file(DOWNLOAD ${archive_url} ${archive_local_path} SHOW_PROGRESS)
+
+        # Check if the download is successful
+        file(SIZE ${archive_local_path} ARCHIVE_FILESIZE)
+        if(0 EQUAL ${ARCHIVE_FILESIZE})
+            file(REMOVE ${archive_local_path})
+            message(FATAL_ERROR "${err_msg}")
+        endif()
+       
+        # Extract
+        message("   Extracting")
+        execute_process(
+            WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
+            COMMAND tar -xf ${archive_local_path}
+        )
+    endif()
+    
+endfunction()
+
+# ----------------------------------------------------------------------------------------------------------------------
+
