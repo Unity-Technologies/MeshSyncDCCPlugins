c14d70933705b0ad63a7c3df8f439e3bfd7d6d8f
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-03-24 19:04:50 +0900

    Fix zstd.cmake to search zstd under downloaded meshsync package

--- Build/cmake_modules/ZSTD.cmake
+++ Build/cmake_modules/ZSTD.cmake
@@ -1,44 +1,56 @@
-# * ZSTD_INCLUDE_DIR
-# * ZSTD_LIBRARY
+# Returns:
+# * ZSTD_INCLUDE_DIR: where zstd.h is found
+# * ZSTD_LIBRARY: zstd library path
 
-find_path(ZSTD_INCLUDE_DIR
-    NAMES
-        "zstd.h"
-    PATHS
-        ${CMAKE_SOURCE_DIR}/External/zstd/include
-)
+function(setup_zstd meshsync_ver)
 
-mark_as_advanced(ZSTD_INCLUDE_DIR)
+    find_path(ZSTD_INCLUDE_DIR
+        NAMES
+            "zstd.h"
+        PATHS
+            ${MESHSYNC_PLUGIN_LOCAL_PATH}/External/zstd/include
+        
+    )
 
-# Decide the name of the zstd lib based on platform
-if(WIN32) 
-    set(zstd_lib_filename "libzstd_static.lib")
-    set(zstd_external_path_suffix "External/zstd/lib/win64")
-elseif(APPLE)
-    set(zstd_lib_filename "libzstd.a")
-    set(zstd_external_path_suffix "External/zstd/lib/osx")
-elseif(LINUX)
-    set(zstd_lib_filename "libzstd.a")
-    set(zstd_external_path_suffix "External/zstd/lib/linux64")
-endif()        
+    mark_as_advanced(ZSTD_INCLUDE_DIR)
+
+    # Decide the name of the zstd lib based on platform
+    if(WIN32) 
+        set(zstd_lib_filename "libzstd_static.lib")
+        set(zstd_lib_path_suffix "win64")
+    elseif(APPLE)
+        set(zstd_lib_filename "libzstd.a")
+        set(zstd_lib_path_suffix "osx")
+    elseif(LINUX)
+        set(zstd_lib_filename "libzstd.a")
+        set(zstd_lib_path_suffix "linux64")
+    endif()        
 
 
-find_file(
-    ZSTD_LIBRARY 
-    NAMES
-        ${zstd_lib_filename}
-    PATHS
-        ${CMAKE_SOURCE_DIR}
-    PATH_SUFFIXES 
-        ${zstd_external_path_suffix}
-    NO_DEFAULT_PATH        
-)
+    find_file(
+        ZSTD_LIBRARY 
+        NAMES
+            ${zstd_lib_filename}
+        PATHS
+            "${MESHSYNC_PLUGIN_LOCAL_PATH}/External/zstd/lib"
+        PATH_SUFFIXES 
+            ${zstd_lib_path_suffix}
+        NO_DEFAULT_PATH        
+    )
 
-mark_as_advanced(ZSTD_LIBRARY)
+    mark_as_advanced(ZSTD_LIBRARY)
 
-include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args("ZSTD"
-    DEFAULT_MSG
-    ZSTD_INCLUDE_DIR
-	ZSTD_LIBRARY
-)
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args("ZSTD"
+        DEFAULT_MSG
+        ZSTD_INCLUDE_DIR
+    	ZSTD_LIBRARY
+    )
+    
+    if(NOT ${ZSTD_FOUND})  
+        message(FATAL_ERROR "zstd could not be found. \n"
+            "Paths searched for ztsd: ${MESHSYNC_PLUGIN_LOCAL_PATH}/External/zstd/lib/${zstd_lib_path_suffix}"
+        )
+    endif()
+    
+endfunction()
