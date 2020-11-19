f26fa2156b437da73c7602faca68a5406551e4ad
sindharta-tanuwijaya <71803280+sindharta-tanuwijaya@users.noreply.github.com>
2020-11-19 23:17:05 +0900

    chore: simplify build pipeline using the plugin setup in MeshSync@0.5.2-preview  (#66)
    * Confirmed to be working using com.unity.meshsync@0.5.2-preview
    * Simplify AddPlugin

--- Plugins~/Build/cmake_modules/MeshSync.cmake
+++ Plugins~/Build/cmake_modules/MeshSync.cmake
@@ -21,11 +21,8 @@ endfunction()
 
 # ------------------------------------------------------------------------------
 
-#return:
-# - MESHSYNC_PLUGIN_SRC_ROOT:   the location of the source code MeshSync plugin. 
 function(setup_meshsync meshsync_ver)     
     set(MESHSYNC_PLUGIN_LOCAL_PATH  "${CMAKE_BINARY_DIR}/MeshSync-${meshsync_ver}/Plugin~")
-    set(MESHSYNC_PLUGIN_SRC_ROOT    "${MESHSYNC_PLUGIN_LOCAL_PATH}/Src" PARENT_SCOPE)
 
     if(ENABLE_ZSTD)    
         include(ZSTD)
