9fcfdbb2c188c1aca76394ce2854eb1470c9fde6
Sindharta Tanuwijaya <sindharta.tanuwijaya@unity3d.com>
2020-11-21 02:05:17 +0900

    Destroy MeshSyncClientBlender context on exit [skip ci]

--- Plugins~/Src/MeshSyncClientBlender/python/2.83.9/unity_mesh_sync.py
+++ Plugins~/Src/MeshSyncClientBlender/python/2.83.9/unity_mesh_sync.py
@@ -266,6 +266,7 @@ class MESHSYNC_OT_ExportCache(bpy.types.Operator):
         layout.prop(self, "strip_normals")
         layout.prop(self, "strip_tangents")
 
+# ---------------------------------------------------------------------------------------------------------------------
 
 classes = (
     MESHSYNC_PT_Main,
@@ -289,6 +290,15 @@ def unregister():
     msb_context.Destroy()
     for c in classes:
         bpy.utils.unregister_class(c)        
+
+
+def DestroyMeshSyncContext():
+    msb_context.Destroy()
+
+import atexit
+atexit.register(DestroyMeshSyncContext)
+
+# ---------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     register()
