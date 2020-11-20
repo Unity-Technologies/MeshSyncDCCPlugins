5d650a5205478b7b9df481d6284693a36456b562
sindharta <sindharta@gmail.com>
2020-11-20 14:33:23 +0900

    Fix crash when closing Blender on Mac [skip ci]

--- Plugins~/Src/MeshSyncClient/msEntityManager.cpp
+++ Plugins~/Src/MeshSyncClient/msEntityManager.cpp
@@ -281,6 +281,8 @@ void EntityManager::setAlwaysMarkDirty(bool v)
 
 void EntityManager::waitTasks()
 {
+    std::unique_lock<std::mutex> lock(m_mutex);
+
     for (auto& p : m_records)
         p.second.waitTask();
 }
