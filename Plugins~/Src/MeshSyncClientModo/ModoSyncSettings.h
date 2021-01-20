f50ee96eea5544810615970e8e969a2e0c171f36
sindharta <sindharta@gmail.com>
2021-01-20 14:30:51 +0900

    Add ModoSyncSettings

---
+++ Plugins~/Src/MeshSyncClientModo/ModoSyncSettings.h
@@ -0,0 +1,16 @@
+﻿#pragma once
+
+#include "MeshSyncClient/BaseSyncSettings.h"
+
+struct ModoSyncSettings : public MeshSyncClient::BaseSyncSettings {
+    float scale_factor = 1.0f;
+    float frame_step = 1.0f;
+    int  timeout_ms = 5000;
+    bool auto_sync = false;
+
+    bool sync_mesh_instances = true;
+    bool sync_replicators = true;
+
+    void validate();
+};
+
