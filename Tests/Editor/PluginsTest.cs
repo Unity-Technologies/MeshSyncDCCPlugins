94248a6aec3ccb29610bf65f1f90773aff8790d0
sindharta <sindharta@gmail.com>
2021-02-03 11:10:51 +0900

    remove obsolete TODO

--- Tests/Editor/PluginsTest.cs
+++ Tests/Editor/PluginsTest.cs
@@ -8,7 +8,6 @@ class PluginsTest {
 	[Test]
 	public void DCCPluginsExist() {
 		
-		//[TODO-sin: 2020-4-9] Check the filename if they have the same version as MeshSync's version
 		string path = Path.Combine("Packages", "com.unity.meshsync.dcc-plugins","Editor","Plugins~");
 		int numFiles = Directory.GetFiles(path, "*", SearchOption.TopDirectoryOnly).Length;
 		Assert.Greater(numFiles,0,"There are no DCC plugins");
