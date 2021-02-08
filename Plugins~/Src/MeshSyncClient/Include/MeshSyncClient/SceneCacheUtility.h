3f69014dd66e1a48b7093ae14b6b04d08ced0606
sindharta <sindharta@gmail.com>
2021-02-08 17:27:56 +0900

    add comment

--- Plugins~/Src/MeshSyncClient/Include/MeshSyncClient/SceneCacheUtility.h
+++ Plugins~/Src/MeshSyncClient/Include/MeshSyncClient/SceneCacheUtility.h
@@ -7,7 +7,10 @@ namespace MeshSyncClient {
 class SceneCacheUtility {
 
 public:
-    //requestedPath: c:/Users/file.txt
+    //requestedPath:
+    //    c:/Users/file.bin
+    //    c:/Users/asset.sc
+    //    c:/Users/asset (sc will be automatically appended)
     static std::string BuildFilePath(const char* requestedPath);
 
 };
