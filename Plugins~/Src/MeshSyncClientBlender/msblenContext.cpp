155bac6386fc67fefeb0a9b198fc8fb1659a6159
sindharta <sindharta@gmail.com>
2021-02-08 10:57:35 +0900

    minor

--- Plugins~/Src/MeshSyncClientBlender/msblenContext.cpp
+++ Plugins~/Src/MeshSyncClientBlender/msblenContext.cpp
@@ -1460,7 +1460,9 @@ bool msblenContext::exportCache(const BlenderCacheSettings& cache_settings) {
     m_settings.curves_as_mesh = cache_settings.curves_as_mesh;
     SettingsUtilities::ApplyCacheToSyncSettings(cache_settings, &m_settings);
 
-    const ms::OSceneCacheSettings oscs = SettingsUtilities::CreateOSceneCacheSettings(frameRate, cache_settings);
+    const ms::OSceneCacheSettings oscs = SettingsUtilities::CreateOSceneCacheSettings(
+        static_cast<float>(frameRate), 
+        cache_settings);
 
     if (!m_cache_writer.open(cache_settings.path.c_str(), oscs)) {
         m_settings = settings_old;
