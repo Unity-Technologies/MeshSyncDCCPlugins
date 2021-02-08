ff4613a52f111e53b2cd7907b7c004288e83ce41
sindharta <sindharta@gmail.com>
2021-02-08 17:15:58 +0900

    Add MeshSyncClientConstants

---
+++ Plugins~/Src/MeshSyncClient/Include/MeshSyncClient/MeshSyncClientConstants.h
@@ -0,0 +1,11 @@
+#pragma once
+
+namespace MeshSyncClient {
+
+class MeshSyncClientConstants {
+public:
+    static constexpr const char* SCENE_CACHE_EXT = "sc";    
+};
+
+
+} // namespace MeshSyncClient
