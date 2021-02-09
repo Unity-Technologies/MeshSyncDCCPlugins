8cb509426b38d782320db5505b29e2b31bbf660b
sindharta <sindharta@gmail.com>
2021-02-09 12:37:44 +0900

    Add a script to help renaming in the test project

---
+++ MeshSyncDCCPlugins~/Assets/Editor/Menu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+public static class Menu {
+    
+    [MenuItem("MeshSyncDCCPlugins/Rename Plugin Zip files")]
+    static void RenamePluginZipFiles() {
+
+        string fullPath = Path.GetFullPath("Packages/com.unity.meshsync.dcc-plugins/Editor/Plugins~");
+
+        var files = Directory.EnumerateFiles(fullPath, "*.zip", SearchOption.TopDirectoryOnly);
+        foreach(string fileName in files) {
+            string[] tokens = fileName.Split('_');
+
+            //already split
+            if (tokens.Length <= 3)
+                continue;
+
+            string destFileName = $"{tokens[0]}_{tokens[2]}_{tokens[3]}";
+            
+            File.Move(fileName, destFileName);
+            Debug.Log($"Moving {fileName} to {destFileName}");
+            
+        }
+    }    
+
+}
