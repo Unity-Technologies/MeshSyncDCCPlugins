e9dba25350a19fc1c0c4caf6b31afa9fb2680603
sindharta <sindharta@gmail.com>
2021-03-29 11:12:51 +0900

    comment

--- Plugins~/Src/MeshSyncClientBlender/msblenContext.cpp
+++ Plugins~/Src/MeshSyncClientBlender/msblenContext.cpp
@@ -1491,11 +1491,11 @@ bool msblenContext::ExportCache(const std::string& path, const BlenderCacheSetti
         // record
         int sceneIndex = 0;
         for (int f = frameStart; f <= frameEnd; f += frameStep) {
-            //scene.frame_set(f);
+            //[Note-sin: 2021-3-29] use Depsgraph.update() to optimize for setting frame (scene.frame_set(f))
             scene.SetCurrentFrame(f);
             bl::BContext::get().EvaluateDepsgraph();
-            m_anim_time = static_cast<float>(f - frameStart) / frameRate;
 
+            m_anim_time = static_cast<float>(f - frameStart) / frameRate;
             DoExportSceneCache(sceneIndex, materialRange, nodes);
             ++sceneIndex;
         }
