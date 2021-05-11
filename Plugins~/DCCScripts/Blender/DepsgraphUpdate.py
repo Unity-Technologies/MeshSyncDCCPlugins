2f6d05a40213c961597cfe81a25d81626a573787
sindharta <sindharta@gmail.com>
2021-05-11 11:55:35 +0900

    Fix depsgraph addon

--- Plugins~/DCCScripts/Blender/DepsgraphUpdate.py
+++ Plugins~/DCCScripts/Blender/DepsgraphUpdate.py
@@ -1,10 +1,11 @@
 bl_info = {
-    "name": "Move X Axis",
+    "name": "Custom Tools",
     "blender": (2, 91, 2),
     "category": "Object",
 }
 
-import bpy
+import bpy;
+import time;
 
 class ToolView:
     bl_space_type = "VIEW_3D"
@@ -24,29 +25,47 @@ class MoveXByOnePanel(ToolView, bpy.types.Panel):
     def draw(self, context):
         scene = bpy.context.scene
         layout = self.layout
-        layout.operator("object.move_x", text="Move X By One")        
+        layout.operator("object.update_depsgraph", text="Update Depsgraph for each frame")        
 
 
-class ObjectMoveX(bpy.types.Operator):
-    """My Object Moving Script"""      # Use this as a tooltip for menu items and buttons.
-    bl_idname = "object.move_x"        # Unique identifier for buttons and menu items to reference.
-    bl_label = "Move X by One"         # Display name in the interface.
-    bl_options = {'REGISTER', 'UNDO'}  # Enable undo for the operator.
+class UpdateDepsgraph(bpy.types.Operator):
+    """Update Depsgraph for each frame"""        # tooltip
+    bl_idname = "object.update_depsgraph"        # Unique ID
+    bl_label = "Update Depsgraph for each frame" # Interface label
+    bl_options = {'REGISTER', 'UNDO'}  
 
-    def execute(self, context):        # execute() is called when running the operator.
+    def execute(self, context):        
+    
+        scene = bpy.context.scene
+        depsgraph = bpy.context.evaluated_depsgraph_get()
+        id_types = ['ACTION', 'ARMATURE', 'BRUSH', 'CAMERA', 'CACHEFILE', 'CURVE', 'FONT', 'GREASEPENCIL', 'COLLECTION', 'IMAGE', 'KEY', 'LIGHT', 'LIBRARY', 'LINESTYLE', 'LATTICE', 'MASK', 'MATERIAL', 'META', 'MESH', 'MOVIECLIP', 'NODETREE', 'OBJECT', 'PAINTCURVE', 'PALETTE', 'PARTICLE', 'LIGHT_PROBE', 'SCENE', 'SIMULATION', 'SOUND', 'SPEAKER', 'TEXT', 'TEXTURE', 'HAIR', 'POINTCLOUD', 'VOLUME', 'WINDOWMANAGER', 'WORLD', 'WORKSPACE']        
 
-        # The original script
-        scene = context.scene
-        for obj in scene.objects:
-            obj.location.x += 1.0
+        for i in range(scene.frame_start, scene.frame_end):
+            startTime = time.time()
+            
+            scene.frame_current = i    
+            depsgraph.update()    
 
-        return {'FINISHED'}            # Lets Blender know the operator finished successfully.
+            for j in id_types:
+                updated = depsgraph.id_type_updated(j)
+                if (updated):
+                    print ("  Change status of %s : %d" % (j, updated))
 
+            endTime = time.time()
+            print("Frame: %s Elapsed Time: %s seconds" % (i, (endTime - startTime)))
+            
+        scene.frame_current = 0    
+        depsgraph.update()
+
+        return {'FINISHED'}   
+
+
+#----------------------------------------------------------------------------------------------------------------------
 
 classes = (
     ToolMainPanel,
     MoveXByOnePanel,
-    ObjectMoveX,
+    UpdateDepsgraph,
 )
 
 def register():
@@ -58,6 +77,8 @@ def unregister():
     for c in classes:
         bpy.utils.unregister_class(c)
 
+
+#----------------------------------------------------------------------------------------------------------------------
 
 if __name__ == "__main__":
     register()
