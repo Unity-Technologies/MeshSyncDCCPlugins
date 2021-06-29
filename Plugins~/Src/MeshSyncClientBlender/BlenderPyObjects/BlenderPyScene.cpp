739c26d4fbf7bfee048bbfb4f100f5951d1ae071
sindharta <sindharta@gmail.com>
2021-06-29 17:24:41 +0900

    fix: the hack to manually update id_type_updated[] for Blender 2.83 [skip ci]

--- Plugins~/Src/MeshSyncClientBlender/BlenderPyObjects/BlenderPyScene.cpp
+++ Plugins~/Src/MeshSyncClientBlender/BlenderPyObjects/BlenderPyScene.cpp
@@ -23,15 +23,20 @@ void BlenderPyScene::SetCurrentFrame(int frame, Depsgraph* depsgraph) {
     SetInt(m_ptr, BlenderPyScene_frame_current, frame);
 
     struct DepsGraphInChar {
-        char Buffer[1240];
+        char Buffer[1240]; //the size is actually different per Blender ver, per OS. What's important is the offset.
     };
+
+#if BLENDER_VERSION <= 283
+    const size_t ID_TYPE_UPDATED_OFFSET = 41;
+#else
+    const size_t ID_TYPE_UPDATED_OFFSET = 265;
+#endif
 
     //[Note-sin: 2021-5-13] Since we are modifying frame_current directly, and not using frame_set(),
     //we have to manually update id_type_updated so that depsgraph.update will invoke the handlers: depsgraph_update_pre, etc
     DepsGraphInChar* charGraph = reinterpret_cast<DepsGraphInChar*>(depsgraph);
 
     //Offset is different between Blender Debug and Release, and maybe different among different Blender versions !
-    const size_t ID_TYPE_UPDATED_OFFSET = 265; 
     charGraph->Buffer[ID_TYPE_UPDATED_OFFSET + INDEX_ID_SCE] = 1;
 
 }
