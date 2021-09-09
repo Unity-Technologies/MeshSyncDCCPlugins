30b27c6bc5e4f223f7f706889f40e82b85be3fc0
Sindharta Tanuwijaya <sindharta@gmail.com>
2021-09-09 13:29:57 +0900

    Add a script to sync package info [skip ci]

---
+++ Scripts~/sync_package_info.sh
@@ -0,0 +1,5 @@
+packageName="com.unity.meshsync-dcc-plugins"
+
+cp package.json "MeshSyncDCCPlugins~\\Packages\\${packageName}"
+cp CHANGELOG.md "MeshSyncDCCPlugins~\\Packages\\${packageName}"    
+
