a4f327f9944c4747c0fabd15e2db899a26b6a2cf
Sindharta Tanuwijaya <sindharta@gmail.com>
2022-02-22 12:08:50 +0900

    add error checking

--- Plugins~/Build/cmake_modules/Python.cmake
+++ Plugins~/Build/cmake_modules/Python.cmake
@@ -86,6 +86,12 @@ function(configure_python python_full_version)
             "  Path: ${PYTHON_SRC_ROOT}"
         )
     endif()    
+
+    if(NOT ${PYTHON_${python_full_version}_LIBRARY})
+        message(FATAL_ERROR "Failed to find and configure Python libraries for ${python_full_version}. \n"
+            "  Path: ${PYTHON_SRC_ROOT}"
+        )
+    endif()
         
 endfunction()
 
