5504b36d667e069793c7ee47d9270bed5d3f7c22
Sean Dillon <sean.dillon@unity3d.com>
2022-04-08 16:50:28 +0100

    Fix: collection visibility

--- Plugins~/Src/MeshSyncClientBlender/msblenContext.cpp
+++ Plugins~/Src/MeshSyncClientBlender/msblenContext.cpp
@@ -294,7 +294,7 @@ void msblenContext::extractTransformData(const Object *obj,
     mu::float3& t, mu::quatf& r, mu::float3& s, ms::VisibilityFlags& vis,
     mu::float4x4 *dst_world, mu::float4x4 *dst_local)
 {
-    vis = { true, visible_in_render(obj), visible_in_viewport(obj) };
+    vis = { visible_in_collection(obj), visible_in_render(obj), visible_in_viewport(obj) };
 
     const mu::float4x4 local = getLocalMatrix(obj);
     const mu::float4x4 world = getWorldMatrix(obj);
@@ -536,8 +536,7 @@ ms::TransformPtr msblenContext::exportReference(Object *src, const DupliGroupCon
     auto assign_base_params = [&]() {
         extractTransformData(src, *dst);
         dst->path = path;
-        // todo:
-        dst->visibility = {};
+        dst->visibility = { visible_in_collection(src), visible_in_render(ctx.group_host), visible_in_viewport(ctx.group_host) };
         dst->world_matrix *= ctx.dst->world_matrix;
     };
 
@@ -603,7 +602,7 @@ ms::TransformPtr msblenContext::exportDupliGroup(const Object *src, const DupliG
 
     std::shared_ptr<ms::Transform> dst = ms::Transform::create();
     dst->path = path;
-    dst->visibility = { true, visible_in_render(ctx.group_host), visible_in_viewport(ctx.group_host) };
+    dst->visibility = { visible_in_collection(src), visible_in_render(ctx.group_host), visible_in_viewport(ctx.group_host) };
 
     const mu::tvec3<float> offset_pos = -get_instance_offset(group);
     dst->position = m_settings.BakeTransform ? mu::float3::zero() : offset_pos;
@@ -618,7 +617,7 @@ ms::TransformPtr msblenContext::exportDupliGroup(const Object *src, const DupliG
         auto obj = go->ob;
         if (auto t = exportObject(obj, true, false)) {
             const bool non_lib = obj->id.lib == nullptr;
-            t->visibility = { true, non_lib, non_lib };
+            t->visibility = { visible_in_collection(obj), non_lib, non_lib };
         }
         exportReference(obj, ctx2);
     }
