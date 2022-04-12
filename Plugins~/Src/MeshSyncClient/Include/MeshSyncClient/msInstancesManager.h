6d50c9744d4d9c3c7c56149aac23d8a77f7e6e06
Sean Dillon <sean.dillon@unity3d.com>
2022-04-12 10:48:51 +0100

    Manager for instanceInfo collection

---
+++ Plugins~/Src/MeshSyncClient/Include/MeshSyncClient/msInstancesManager.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#include "MeshSync/MeshSync.h"
+#include<map>
+#include <unordered_map>
+#include "msTransformManager.h"
+
+#ifndef msRuntime
+
+msDeclClassPtr(InstanceInfo)
+
+namespace ms {
+
+struct InstancesManagerRecord
+{
+    bool dirtyInstances = false;
+    bool dirtyMesh = false;
+    InstanceInfoPtr instances = nullptr;
+    TransformPtr entity = nullptr;
+    bool updated = false;
+};
+
+/// <summary>
+/// Manager for transforms and transform instances.
+/// </summary>
+class InstancesManager : public TransformManager<InstancesManagerRecord>
+{
+public:
+    /// <summary>
+    /// Returns meshes that have changed since the last export.
+    /// </summary>
+    std::vector<TransformPtr> getDirtyMeshes();
+
+    /// <summary>
+    /// Returns instaces that have changed since the last export.
+    /// </summary>
+    std::vector<InstanceInfoPtr> getDirtyInstances();
+
+    /// <summary>
+    /// Returns identifiers of records that have been deleted since the last export.
+    /// </summary>
+    std::vector<Identifier>& getDeleted();
+
+    /// <summary>
+    /// Clears the dirty flags on the records.
+    /// </summary>
+    void clearDirtyFlags();
+
+    /// <summary>
+    /// Adds a record about instancing information.
+    /// </summary>
+    void add(InstanceInfoPtr instanceInfo);
+
+    /// <summary>
+    /// Adds or updates a record about the transform.
+    /// </summary>
+    void add (TransformPtr entity) override;
+
+    /// <summary>
+    /// Clears the records collection.
+    /// </summary>
+    void clear() override;
+
+    /// <summary>
+    /// Touches the record at given path. 
+    /// The record will not be considered stale
+    /// at the end of exportation.
+    /// </summary>
+    void touch(const std::string& path) override;
+
+    /// <summary>
+    /// Erases records that have not 
+    /// been added or touched in the last exportation.
+    /// </summary>
+    void eraseStaleEntities() override;
+};
+
+
+
+} // namespace ms
+#endif // msRuntime
