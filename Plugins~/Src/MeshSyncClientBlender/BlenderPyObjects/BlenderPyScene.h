58cc3d94a15aa5da59f55b7fba19d037c702e1d5
Christian Schinkoethe <christian.schinkoethe@unity.com>
2022-06-28 14:26:55 +0100

    Reverting format fixes

--- Plugins~/Src/MeshSyncClientBlender/BlenderPyObjects/BlenderPyScene.h
+++ Plugins~/Src/MeshSyncClientBlender/BlenderPyObjects/BlenderPyScene.h
@@ -15,18 +15,18 @@ public:
     MSBLEN_BOILERPLATE2(BlenderPyScene, Scene)
     MSBLEN_COMPATIBLE(BlenderPyID)
 
-    int fps() const;
-    int frame_start() const;
-    int frame_end() const;
+    int fps() const ;
+    int frame_start() const ;
+    int frame_end() const ;
     int GetCurrentFrame() const;
     void SetCurrentFrame(int frame, Depsgraph* depsgraph);
 
     void frame_set(int f, float subf = 0.0f);
 
     template<class Body>
-    void each_objects_impl(const Body& body, CollectionChild* cc)
+    void each_objects_impl(const Body& body, CollectionChild *cc)
     {
-        for (auto* c : list_range(cc)) {
+        for (auto *c : list_range(cc)) {
             each_objects_impl(body, (CollectionChild*)c->collection->children.first);
             for (auto* o : list_range((CollectionObject*)c->collection->gobject.first))
                 body(o->ob);
@@ -37,7 +37,7 @@ public:
     void each_objects(const Body& body)
     {
         each_objects_impl(body, (CollectionChild*)m_ptr->master_collection->children.first);
-        for (auto* o : list_range((CollectionObject*)m_ptr->master_collection->gobject.first))
+        for (auto *o : list_range((CollectionObject*)m_ptr->master_collection->gobject.first))
             body(o->ob);
     }
 
