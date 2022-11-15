358985a4e010fc51370b7325319a8154a30f80e8
seandillon92 <sean.dillon@unity3d.com>
2022-11-15 10:22:43 +0000

    blender: use hub to select or create project (#297)
    * Use unity hub to select project
    
    * Removed prints
    
    * Support for creating project
    
    * threading improvements: use deamon threads, utilise atexit event to attempt gracefull shutdown, use 1s timeout for thread join

--- Plugins~/Src/MeshSyncClientBlender/python/unity_mesh_sync_preferences.py
+++ Plugins~/Src/MeshSyncClientBlender/python/unity_mesh_sync_preferences.py
@@ -1,11 +1,14 @@
 import bpy
 import os
 import platform
+import atexit
 
 from .unity_mesh_sync_installation import *
 from . import MeshSyncClientBlender as ms
 from bpy.types import AddonPreferences
 from bpy.props import StringProperty, IntProperty, BoolProperty
+from threading import Thread
+from time import sleep
 
 msb_context = ms.Context()
 
@@ -18,6 +21,85 @@ def msb_get_editor_path_prefix_default():
         path = "/Applications/Unity/Hub/Editor"
     return path
 
+class MESHSYNC_OT_OpenHub(bpy.types.Operator):
+    bl_idname = "meshsync.open_hub"
+    bl_label = "Select or Create Project with Unity Hub"
+    active = False
+    logs_thread = None
+    thread_timeout = 1.0
+
+    @classmethod
+    def description(cls, context, properties):
+        return "Open Unity Hub to select or create a Unity project to sync data to"
+
+    def invoke(self, context, event):
+
+        if MESHSYNC_OT_OpenHub.active:
+            MESHSYNC_OT_OpenHub.active = False
+            if self.logs_thread is not None:
+                self.logs_thread.join(MESHSYNC_OT_OpenHub.thread_timeout)
+                atexit.unregister(MESHSYNC_OT_OpenHub.shutdown_thread)
+        else:
+            if self.logs_thread is not None:
+                self.logs_thread.join(MESHSYNC_OT_OpenHub.thread_timeout)
+            MESHSYNC_OT_OpenHub.active = True
+
+            self.logs_thread = Thread(target = self.monitor_logs, args = (context,), daemon = True)
+            self.logs_thread.start()
+            
+            atexit.unregister(MESHSYNC_OT_OpenHub.shutdown_thread)
+            atexit.register(MESHSYNC_OT_OpenHub.shutdown_thread)
+
+            hub_path = msb_preferences(context).hub_path
+            subprocess.Popen([hub_path])
+
+        return {'FINISHED'}
+
+    def shutdown_thread():
+        MESHSYNC_OT_OpenHub.active = False
+        if MESHSYNC_OT_OpenHub.logs_thread is not None:
+            MESHSYNC_OT_OpenHub.logs_thread.join(MESHSYNC_OT_OpenHub.thread_timeout)
+
+    def monitor_logs(self, context):
+        logs_path = os.path.join(msb_get_hub_dir(), "logs", "info-log.json")
+        with open(logs_path, "r+") as log_file:
+            log_file.seek(0, os.SEEK_END)
+
+            while MESHSYNC_OT_OpenHub.active:
+                line = log_file.readline()
+                while len(line) > 0:
+                    if not MESHSYNC_OT_OpenHub.active:
+                        return
+
+                    MESHSYNC_OT_OpenHub.handle_log_entry(context, line)
+                    line = log_file.readline()
+
+                #if idle, sleep for 0.1 seconds
+                sleep(0.1)
+
+    def handle_log_entry(context, line):
+        path = MESHSYNC_OT_OpenHub.extract_path_from_log_entry(line, 'openProject', 'openProject projectPath: (.*), current editor:')
+        if path is not None:
+            msb_preferences(context).project_path = path
+            return
+
+        path  = MESHSYNC_OT_OpenHub.extract_path_from_log_entry(line, 'ALREADY_OPEN', '\"projectPath\":\"(.*)\"')
+        if path is not None:
+            msb_preferences(context).project_path = path
+            return
+
+        path = MESHSYNC_OT_OpenHub.extract_path_from_log_entry(line, 'createProject', 'createProject projectPath: (.*), editor version:')
+        if path is not None:
+            msb_preferences(context).project_path = path
+
+    def extract_path_from_log_entry(line, token, regex):
+        if token in line:
+            message = json.loads(line)['message']
+            result = re.search(regex, message)
+            if result is not None:
+                return os.path.normpath(result.group(1))
+
+
 class MESHSYNC_OT_ResetPreferences(bpy.types.Operator):
     bl_idname = "meshsync.reset_preferences"
     bl_label = "Reset Preferences"
@@ -27,8 +109,7 @@ class MESHSYNC_OT_ResetPreferences(bpy.types.Operator):
         return "Find where the Unity Hub and the Unity Editors are located in the system"
 
     def execute(self, context):
-        preferences = context.preferences.addons[__package__].preferences
-        preferences.reset()
+        msb_preferences(context).reset()
         return {'FINISHED'}
 
 class MESHSYNC_Preferences(AddonPreferences):
@@ -45,17 +126,32 @@ class MESHSYNC_Preferences(AddonPreferences):
 
     def is_hub_installed():
         path = msb_get_hub_path()
-        return dir is not None and os.path.exists(path)
+        return os.path.exists(path)
+
+    def is_hub_supported():
+        hub_version = msb_get_hub_version()
+        if hub_version == "":
+            return False
+
+        return msb_get_most_recent_version(hub_version, "3.0.0") == hub_version
 
     def reset(self):
         self.hub_path = msb_get_hub_path()
-        self.editors_path = msb_get_editors_path()
+        self.hub_version = msb_get_hub_version()
         self.hub_installed = MESHSYNC_Preferences.is_hub_installed()
+        self.hub_supported = MESHSYNC_Preferences.is_hub_supported()
 
-    project_path: StringProperty(name = "Unity Project", default= "C:/", subtype = 'DIR_PATH')
-    editors_path: bpy.props.StringProperty(name = "Unity Editors", default= msb_get_editor_path_prefix_default(), subtype = 'DIR_PATH')
-    hub_path: bpy.props.StringProperty(name = "Unity Hub", default = msb_get_hub_path(), subtype = 'FILE_PATH')
-    hub_installed : bpy.props.BoolProperty(name = "Hub Installed", default = is_hub_installed())
+        self.editors_path = msb_get_editors_path()
+
+    def redraw(self, context):
+        regions = context.area
+        if regions == None:
+            return None
+
+        for region in context.area.regions:
+            if region.type == "UI":
+                region.tag_redraw()
+        return None
 
     def draw(self, context):
         layout = self.layout
@@ -81,8 +177,25 @@ class MESHSYNC_Preferences(AddonPreferences):
         project_layout.label(text ="Project Settings")
         project_layout.prop(self, "project_path")
 
+        if self.hub_supported:
+            row = project_layout.row()
+            if MESHSYNC_OT_OpenHub.active:
+                row.operator("meshsync.open_hub", text = "Stop using Unity Hub", icon = 'PAUSE')
+            else:
+                row.operator("meshsync.open_hub", text = "Select or Create project via Unity Hub", icon = 'PLAY')
+
+
     def register():
         bpy.utils.register_class(MESHSYNC_OT_ResetPreferences)
+        bpy.utils.register_class(MESHSYNC_OT_OpenHub)
 
     def unregister():
         bpy.utils.unregister_class(MESHSYNC_OT_ResetPreferences)
+        bpy.utils.unregister_class(MESHSYNC_OT_OpenHub)
+
+    project_path: StringProperty(name = "Unity Project", default= "C:/", subtype = 'DIR_PATH', update = redraw)
+    editors_path: bpy.props.StringProperty(name = "Unity Editors Location", default= msb_get_editor_path_prefix_default(), subtype = 'DIR_PATH')
+    hub_path: bpy.props.StringProperty(name = "Unity Hub Location", default = msb_get_hub_path(), subtype = 'FILE_PATH')
+    hub_version = bpy.props.StringProperty(name = "Unity Hub Version", default = msb_get_hub_version())
+    hub_installed : bpy.props.BoolProperty(name = "Hub Installed", default = is_hub_installed())
+    hub_supported: bpy.props.BoolProperty(name = "Hub Supported", default = is_hub_supported())
