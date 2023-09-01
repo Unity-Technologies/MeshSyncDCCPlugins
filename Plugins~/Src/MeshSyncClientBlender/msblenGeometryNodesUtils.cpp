dd2c08bc50f081d6a600249f345039553b11f65d
Christian Schinkoethe <christian.schinkoethe@unity.com>
2023-09-01 11:36:49 +0100

    Fix for instances not being sent

--- Plugins~/Src/MeshSyncClientBlender/msblenGeometryNodesUtils.cpp
+++ Plugins~/Src/MeshSyncClientBlender/msblenGeometryNodesUtils.cpp
@@ -333,10 +333,10 @@ namespace blender {
                 }
             }
 
-
             // Instances can be hidden if they reference a mesh without vertices, even if the original mesh has verts.
-            // If that happens, don't pass the instance on:
-            if (object->type == OB_MESH) {
+            // If that happens, don't pass the instance on, unless it's a parent of another instance:
+            if (object->type == OB_MESH &&
+                 instanceParentsToChildren.count(objectName) == 0) {
                 auto mesh = (Mesh*)object->data;
                 if (mesh == nullptr || mesh->totvert == 0) {
                     continue;
